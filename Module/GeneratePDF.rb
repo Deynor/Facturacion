2d8c566df6cd063019b10c349ad9f1e220ac7a67
Deynor <deynor05@gmail.com>
2022-10-13 13:48:43 -0600

    Generador PDF
    Esta clase se encarga de generar los documentos PDF

---
+++ Module/GeneratePDF.rb
@@ -0,0 +1,20 @@
+#----Autor: Deynor Rodríguez Rojas
+#----Fecha de creación: 13 de octubre del 2022
+#----Cambios realizados:
+#----Fecha         /        Autor         /   Cambio realizado
+#---- 13/10/2022   / Christopher Land     / Creado el formato de los comentarios
+
+require 'prawn'
+
+class GeneratePDF
+  
+  #----Variables
+  
+  direccionGuardar = ""
+  
+  #----Creación de PDF
+  Prawn::Document.generate("hello.pdf") do
+    text "Hello World 2!"
+  end
+  
+end#Fin de la clase GeneratePDF
