68359c996914d25078bbf7d9a978c59383e3026e
chris <christopher.top3@gmail.com>
2022-10-14 18:53:41 -0600

    Avances en Calculos.rb
    Existe un trabe al intentar consultar el json
    
    A pesar de estar bien hecho, da errores de json dado invalido

--- Experimentation/Test.rb
+++ Experimentation/Test.rb
@@ -3,21 +3,9 @@
 #----Descripción: Está es una clase para realizar pruebas y siempre se va a encontrar en constante cambio por lo que no necesita guardar sus cambios realizados.
 
 require "Variables/Variables"
+require "Module/Calculos"
+require 'json'
 
-var = []
+hash = JSON.parse($json_Record[0])
 
-case true
-when var.instance_of?(Integer) #Revisando si es integer
-  puts "Es Integer"
-when var.instance_of?(String) #Revisando si es String
-  puts "Es String"
-when var.instance_of?(Array) #Revisando si es Array
-  puts "Es Array"
-end#end of case true for var
-
-#puts "Revisando si es integer"
-#puts var.instance_of?(Integer) #Revisando si es integer
-#puts "Revisando si es String"
-#puts var.instance_of?(String) #Revisando si es String
-#puts "Revisando si es Array"
-#puts var.instance_of?(Array) #Revisando si es Array
+puts hash
--- Module/Calculos.rb
+++ Module/Calculos.rb
@@ -2,22 +2,66 @@
 #----Fecha de creación: 13 de octubre del 2022
 #----Cambios realizados:
 #----Fecha         /        Autor         /   Cambio realizado
+#---- 14/10/2022   / Christopher Land     / Creado el método "RevisarTipo(var)" y se completa lógica de "Agregar Ceros"
 
 class Calculos
-  
-  def agregarZeros(num,cantidadZeros)
+  def agregarCeros(num,cantidadCeros)
+
+    #----Variables
+    temp = ""
+    tipoNum = RevisarTipo(num)
+    mensajes = [["agregó", "agregaron"],["cero", "ceros"]]
+    mensajeEscogido = 0
     
+    #----Mensaje
+    puts "Para agregar ceros se necesita usar un string"
+    
+    #----Convirtiendo valor recibido
+    case tipoNum
+    when 0
+      puts "Convirtiendo a String"
+      num = num.to_s
+    when 1
+      puts "No es necesario convertir"
+    when 2
+      puts "Tomando el último valor del Array, se da por sentado que es un array de un solo indice"
+      num = num.last()
+    end#Fin del case tipoNum
+    
+    #----Agregando los ceros
+    for i in 1..cantidadCeros
+      temp = "#{temp}0"
+    end#Fin del for
+    
+    #----Agregando número a la derecha de los ceros
+    num = "#{temp}#{num}"
+    
+    #----Mensaje de resultado Final
+    if(cantidadCeros > 1)
+      mensajeEscogido = 1
+    else
+      mensajeEscogido = 0
+    end#Fin del if
+    puts "Al número se le #{mensajes[0][mensajeEscogido]} #{cantidadCeros} #{mensajes[1][mensajeEscogido]} quedando como: #{num}"
+
+    return num #Regresando el número ya con los ceros agregados
+  end#Fin del método agregarZeros
+
+  def RevisarTipo(var)
+
     #----Revisando tipo de dato enviado
     case true
     when var.instance_of?(Integer) #Revisando si es integer
-      puts "Haz pasado un número, transformandolo a String"
+      puts "Haz pasado un número"
+      return 0
     when var.instance_of?(String) #Revisando si es String
-      puts "Haz pasado un string, No es necesario transformarlo"
+      puts "Haz pasado un string"
+      return 1
     when var.instance_of?(Array) #Revisando si es Array
-      puts "Haz pasado un Array, Tomando el último valor"
-      puts "Nota: Se da por hecho que el array es de solo un indice."
+      puts "Haz pasado un Array"
+      return 2
     end#end of case true for num
-    
-  end#Fin del método agregarZeros
-  
+
+  end#Fin del método RevisarTipo
+
 end#Fin de la clase Calculos
